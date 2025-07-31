b48ee7fad4ae228e0d36fc32fa92931927ab5f1a
Shumani.Monyai <Shumani.Monyai2@gmail.com>
2025-07-31 17:44:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.hero .container', { delay: 200, origin: 'bottom', distance: '50px' });
+ScrollReveal().reveal('.about h2, .about p', { delay: 300, origin: 'bottom', distance: '30px' });
+ScrollReveal().reveal('.card', { interval: 200, origin: 'bottom', distance: '40px' });
+ScrollReveal().reveal('.newsletter h2, .newsletter p, .newsletter form', { delay: 400, origin: 'bottom', distance: '30px' });
